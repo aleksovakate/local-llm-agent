43179a8f6d79359a6c11dc47563bb306b5c16271
Katerina Aleksova <katerina.aleksova@stud.th-deg.de>
2026-06-21 19:54:19 +0200

    Ollama test

--- agent.py
+++ agent.py
@@ -1,1 +1,9 @@
-# Local LLM agent
+from langchain_ollama import ChatOllama
+
+# creates model
+model = ChatOllama(model="llama3.2:3b")
+
+# send question
+response = model.invoke("What is the capital of North Macedonia?")
+
+print(response.content)
